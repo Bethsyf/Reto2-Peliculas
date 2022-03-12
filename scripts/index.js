230be4a3c0d5ead4dcf39d8bcbc204f90c578055
Bethsyf <bethsyfalcon@gmail.com>
2022-03-12 18:40:57 -0500

    slidercon JS terminado

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider-section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
-let btnLeft = document.querySelector('btn-left');
-let btnRight = document.querySelector('btn-right')
+let btnLeft = document.querySelector('#btn-left');
+let btnRight = document.querySelector('#btn-right');
 
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
@@ -11,8 +11,20 @@ function Next(){
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
-        slider.style.transition = "none"
+        slider.style.transition = "none";
         slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
         slider.style.marginLeft = "-100%";
     }, 500);
 }
@@ -20,3 +32,11 @@ function Next(){
 btnRight.addEventListener('click', function(){
     Next();
 });
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000);
