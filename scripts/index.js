30566152ced12b221b80d7d0d0611399a1542f88
Bethsyf <bethsyfalcon@gmail.com>
2022-03-12 20:38:36 -0500

    insertar elementos desde Js

--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,30 @@ btnLeft.addEventListener('click', function(){
 setInterval(function(){
     Next();
 }, 5000);
+
+let image1 = document.createElement('img');
+image1.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img1').appendChild(image1);
+let tituloImage1 = document.createElement('h3');
+tituloImage1.textContent = 'Abominable';
+document.querySelector('#img1').appendChild(tituloImage1);
+
+let image2 = document.createElement('img');
+image2.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img2').appendChild(image2);
+
+let image3 = document.createElement('img');
+image3.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img3').appendChild(image3);
+
+let image4 = document.createElement('img');
+image4.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img4').appendChild(image4);
+
+let image5 = document.createElement('img');
+image5.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img5').appendChild(image5);
+
+let image6 = document.createElement('img');
+image6.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+document.querySelector('#img6').appendChild(image6);
