517f08e5710dc6f0e7202ed0f3758976d3cf0428
Bethsyf <bethsyfalcon@gmail.com>
2022-03-18 07:54:00 -0500

    ajustes para mostrar peliculas

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import {cards} from "../data/data.js";
-import {detailCard} from "./modules/detailCard";
+import {showMovies} from "./modules/showMovies.js";
 let slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider-section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
@@ -43,32 +43,37 @@ setInterval(function(){
     Next();
 }, 5000);
 
-let image1 = document.createElement('img');
-image1.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
-document.querySelector('#img1').appendChild(image1);
-let tituloImage1 = document.createElement('h3');
-tituloImage1.textContent = ' Un Amigo Abominable';
-document.querySelector('#img1').appendChild(tituloImage1);
+let contMovies = document.getElementById('contMovies');
+document.addEventListener('DOMContentLoaded', () => {
+    showMovies(cards, contMovies)
+})
 
-let image2 = document.createElement('img');
-image2.src = 'https://es.web.img2.acsta.net/pictures/19/01/14/13/23/5258040.jpg';
-document.querySelector('#img2').appendChild(image2);
-let tituloImage2 = document.createElement('h3');
-tituloImage2.textContent = ' IO, sola en la tierra';
-document.querySelector('#img2').appendChild(tituloImage2);
+// let image1 = document.createElement('img');
+// image1.src = 'https://pics.filmaffinity.com/Un_amigo_abominable-320900881-large.jpg';
+// document.querySelector('#img1').appendChild(image1);
+// let tituloImage1 = document.createElement('h3');
+// tituloImage1.textContent = ' Un Amigo Abominable';
+// document.querySelector('#img1').appendChild(tituloImage1);
+
+// let image2 = document.createElement('img');
+// image2.src = 'https://es.web.img2.acsta.net/pictures/19/01/14/13/23/5258040.jpg';
+// document.querySelector('#img2').appendChild(image2);
+// let tituloImage2 = document.createElement('h3');
+// tituloImage2.textContent = ' IO, sola en la tierra';
+// document.querySelector('#img2').appendChild(tituloImage2);
 
 
-let image3 = document.createElement('img');
-image3.src = 'https://pics.filmaffinity.com/El_proyecto_Adam-718096348-large.jpg';
-document.querySelector('#img3').appendChild(image3);
-let tituloImage3 = document.createElement('h3');
-tituloImage3.textContent = 'El proyecto Adam';
-document.querySelector('#img3').appendChild(tituloImage3);
+// let image3 = document.createElement('img');
+// image3.src = 'https://pics.filmaffinity.com/El_proyecto_Adam-718096348-large.jpg';
+// document.querySelector('#img3').appendChild(image3);
+// let tituloImage3 = document.createElement('h3');
+// tituloImage3.textContent = 'El proyecto Adam';
+// document.querySelector('#img3').appendChild(tituloImage3);
 
-let image4 = document.createElement('img');
-image4.src = 'https://m.media-amazon.com/images/I/51vSMi5ABJL.jpg';
-document.querySelector('#img4').appendChild(image4);
-let tituloImage4 = document.createElement('h3');
-tituloImage4.textContent = 'Fin de semana en Croacia';
-document.querySelector('#img4').appendChild(tituloImage4);
+// let image4 = document.createElement('img');
+// image4.src = 'https://m.media-amazon.com/images/I/51vSMi5ABJL.jpg';
+// document.querySelector('#img4').appendChild(image4);
+// let tituloImage4 = document.createElement('h3');
+// tituloImage4.textContent = 'Fin de semana en Croacia';
+// document.querySelector('#img4').appendChild(tituloImage4);
 
