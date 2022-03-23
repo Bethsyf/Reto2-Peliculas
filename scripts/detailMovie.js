8fa2a87f95492af517c5ee95670c0c703f48e29f
Bethsyf <bethsyfalcon@gmail.com>
2022-03-23 00:21:04 -0500

    descomentar

--- scripts/detailMovie.js
+++ scripts/detailMovie.js
@@ -1,15 +1,15 @@
 let titleMovie = document.querySelector('h3');
-// let image = document.querySelector('img');
-// let details = document.querySelector('p');
-// let button = document. querySelector('button');
+let image = document.querySelector('img');
+let details = document.querySelector('p');
+
 
 document.addEventListener('DOMContentLoaded', () => {
     let OBJECT_CARD1 = JSON.parse(localStorage.getItem('ObjectCard'));
     let [name, img, description ] =  OBJECT_CARD1;
     titleMovie.textContent = name;
-    // image.setAttribute('src', img);
-    // image.setAttribute('alt', name);
-    // details.textContent = description    
+    image.setAttribute('src', img);
+    image.setAttribute('alt', name);
+    details.textContent = description    
 })
 
 
